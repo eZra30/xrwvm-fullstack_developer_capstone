10d5dd156821e483556f28cf73d689bf53dea4eb
eZra30 <105845519+eZra30@users.noreply.github.com>
2024-08-19 22:45:22 +0800

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,20 +1,21 @@
 from .models import CarMake, CarModel
 
 
-def initiate():
-    car_make_data = [
-        {"name": "NISSAN", "description": "Great cars. Japanese technology"},
-        {"name": "Mercedes", "description": "Great cars. German technology"},
-        {"name": "Audi", "description": "Great cars. German technology"},
-        {"name": "Kia", "description": "Great cars. Korean technology"},
-        {"name": "Toyota", "description": "Great cars. Japanese technology"},
-    ]
+# def initiate():
+#     car_make_data = [
+#         {"name": "NISSAN", "description": "Great cars. Japanese technology"},
+#         {"name": "Mercedes", "description": "Great cars. German technology"},
+#         {"name": "Audi", "description": "Great cars. German technology"},
+#         {"name": "Kia", "description": "Great cars. Korean technology"},
+#         {"name": "Toyota", "description": "Great cars. Japanese technology"},
+#     ]
 
-car_make_instances = []
+
+# car_make_instances = []
 for data in car_make_data:
     car_make_instances.append(
-       CarMake.objects.create(name=data['name'], 
-       description=data['description'])
+        CarMake.objects.create(name=data['name'], 
+        description=data['description'])
     )
 
 # Create CarModel instances with the corresponding CarMake instances
@@ -54,6 +55,6 @@ car_model_data = [
 
 
 for data in car_model_data:
-    CarModel.objects.create(name=data['name'], 
+    CarModel.objects.create(name=data['name'],
     car_make=data['car_make'], type=data['type'], year=data['year'])
     
