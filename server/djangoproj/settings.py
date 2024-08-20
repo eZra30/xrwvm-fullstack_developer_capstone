1bb09ff0c3cdf78bee7ec83c5765ad45b406d9b6
eZra30 <105845519+eZra30@users.noreply.github.com>
2024-08-20 08:37:57 +0800

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,12 +30,19 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://ezraearlsegu-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    (
+        'https://ezraearlsegu-8000.theiadockernext-0-labs-prod-'
+        'theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    )
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://ezraearlsegu-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    (
+        'https://ezraearlsegu-8000.theiadockernext-0-labs-prod-'
+        'theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    )
 ]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -99,22 +106,31 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-      'NAME':
-      'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
-      'NAME':
-      'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
+        ),
     },
     {
-      'NAME':
-      'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator'
+        ),
     },
     {
-      'NAME':
-      'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator'
+        ),
     },
 ]
+
 
 
 # Internationalization
